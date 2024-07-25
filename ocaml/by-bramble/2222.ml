ee6e5a6eb66c371f9ea95558b07e7d862a8bdd7e
Shadow Kestrel <git-contact@mail.kestrel.gay>
2024-07-25 17:04:45 +0100

    abstract the lines reader over filename

--- ocaml/by-bramble/2222.ml
+++ ocaml/by-bramble/2222.ml
@@ -1,7 +1,14 @@
-let print_and_wait word = print_endline word ; Unix.sleepf 0.6039604
+let print_and_wait word =
+  print_string word ;
+  flush stdout ;
+  Unix.sleepf 0.6039604 ;
+  print_string "\b\b\b\b" ;
+  flush stdout
 
-let words_file = In_channel.open_text "2222.txt"
-let words = In_channel.input_lines words_file
-In_channel.close words_file
+let words_from filename =
+  let words_file = In_channel.open_text filename in
+  let to_ret = In_channel.input_lines words_file in
+  let _ = In_channel.close words_file in
+  to_ret
 
-let _ = List.iter print_and_wait words
+let _ = List.iter print_and_wait (words_from "2222.txt")
