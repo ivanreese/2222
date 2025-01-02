8a1a8a005e9fcd1a5e3e3107e2b678785bb6c360
whc2001 <ianwang0122@outlook.com>
2025-01-01 22:07:45 -0500

    fix(csharp): Open dict file from current directory

--- csharp/by-whc2001/Program.cs
+++ csharp/by-whc2001/Program.cs
@@ -1,1 +1,1 @@
-﻿File.ReadAllLines("../../../../2222.txt").ToList().ForEach((line) => { Console.Clear(); Console.WriteLine(line); Thread.Sleep(TimeSpan.FromTicks(6039604)); });
+﻿File.ReadAllLines("2222.txt").ToList().ForEach((line) => { Console.Clear(); Console.WriteLine(line); Thread.Sleep(TimeSpan.FromTicks(6039604)); });
